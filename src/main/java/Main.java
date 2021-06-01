849d8f693d6eedd97b7211b342c49a654d13252f
David Santamaria <davidsantamaria1245@gmail.com>
2021-06-01 16:26:09 -0400

    upload files

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a noun:");
+        String noun = in.next();
+        System.out.print("Enter a verb:");
+        String verb = in.next();
+        System.out.print("Enter an adjective:");
+        String adjective = in.next();
+        System.out.print("Enter an adverb:");
+        String adverb = in.next();
+        System.out.println("Do you walk your " + adjective + " " + noun + " " + adverb + "? That's hilarious!");
+    }
+}
